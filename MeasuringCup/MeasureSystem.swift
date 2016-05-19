68c6c5c625e06da0b83c5457ef97f500dc9d99df
Andrés Catalán <a@catalandr.es>
2016-05-19 16:51:42 -0400

    Added British measures in mass and (liquid) volume

---
+++ MeasuringCup/MeasureSystem.swift
@@ -0,0 +1,14 @@
+//
+//  MeasureSystem.swift
+//  MeasuringCup
+//
+//  Created by Andrés Catalán on 2016–05–19.
+//  Copyright © 2016 Ayre. All rights reserved.
+//
+
+import Foundation
+
+public enum MeasureSystem {
+    case british
+    case american
+}
